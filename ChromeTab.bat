c98014dc8c84e1acf405edf401f7297a51922b3e
Anmol Agarwal <53789745+Anmol17Agarwal@users.noreply.github.com>
2023-08-25 11:15:04 +0530

    Update ChromeTab.bat
    Make changes to simple tab

--- ChromeTab.bat
+++ ChromeTab.bat
@@ -2,5 +2,5 @@
 start https://github.com/anmol17agarwal
 start https://outlook.office.com/mail/
 start https://myportal.peoplestrong.com/oneweb/#/home
-start https://bard.google.com/
-start https://stackoverflow.com/questions/52164599/spring-boot-field-required-a-bean-of-type-that-could-not-be-found
+start https://mail.google.com/mail/
+start https://www.facebook.com/login/
