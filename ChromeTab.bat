e15f6d5baae1cb2ac35f621cc663bc08203d0906
Anmol Agarwal <53789745+Anmol17Agarwal@users.noreply.github.com>
2023-08-27 00:15:28 +0530

    Update ChromeTab.bat

--- ChromeTab.bat
+++ ChromeTab.bat
@@ -1,6 +1,5 @@
 @echo off
 start https://github.com/anmol17agarwal
-start https://outlook.office.com/mail/
-start https://myportal.peoplestrong.com/oneweb/#/home
+start https://outlook.office.com
 start https://mail.google.com/mail/
 start https://www.facebook.com/login/
